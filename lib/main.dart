493a4ad9eafe0d5ac04d3421a26f5506f7829816
ashishhiremath72@gmail.com <ashishhiremath72@gmail.com>
2020-05-18 19:48:32 +0530

    changed background

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: Container(
+        color: Colors.white,
+      ),
+    );
   }
 }
